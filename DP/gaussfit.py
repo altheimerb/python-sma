bf9b27f27ab389fa3f270241a86e601c4e331ce4
Ben Altheimer <Ben@CajalDomain.local>
2018-09-28 10:53:04 -0400

    constrain gaussian fit sigma > one bin width

--- DP/gaussfit.py
+++ DP/gaussfit.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 def fitgauss(bin_edges, counts, guess):
 	"""fit offset gaussian, with offset, amplitude, and sigma constrained to be positive"""
+	"""sigma is constrained to be >= one bin width"""
 	"""unless guess[3] < 0, then offset set to zero"""
 	bincen = np.zeros(np.size(bin_edges)-1)
 	bincen[:] = bin_edges[0:np.size(bin_edges)-1]
@@ -19,11 +20,11 @@ def fitgauss(bin_edges, counts, guess):
 
 	if(guess[3]>0): #allow y_offset
 		coeff, var_matrix = curve_fit(gauss, bincen, counts, guess, 
-			bounds=([0,-np.inf, 0, 0],[np.inf,np.inf, np.inf, np.inf]))
+			bounds=([0,-np.inf, binstep, 0],[np.inf,np.inf, np.inf, np.inf]))
 		[A, mu, sigma, yoff] = coeff
 	else: #force y_offset to be zero - pure gaussian
 		coeff, var_matrix = curve_fit(puregauss, bincen, counts, guess[0:3], 
-			bounds=([0,-np.inf, 0],[np.inf,np.inf, np.inf]))
+			bounds=([0,-np.inf, binstep],[np.inf,np.inf, np.inf]))
 		[A, mu, sigma] = coeff
 		yoff = 0.0
 	c_exp = gauss(bincen, A, mu, sigma, yoff)
